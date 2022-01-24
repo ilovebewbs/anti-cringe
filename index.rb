8477cb7eb436ce2f56baf5b233134c3d907609d0
ilovebewbs <sruwhfew@gmail.com>
2022-01-24 16:40:38 +0300

    fixed stuff

--- index.rb
+++ index.rb
@@ -1,10 +1,11 @@
 require 'discordrb'
+require 'dotenv/load'
 require './handlers/messages'
-
-bot = Discordrb::Bot.new token: 'YOUR_TOKEN_HERE'
+bot = Discordrb::Bot.new token:ENV['BOT_TOKEN']
 
 no_words = ['bitcoin','btc','nft','NFT','BTC','crypto','CRYPTO']
 
 Messages.detect_cringe(no_words,bot)
 
 bot.run();
+
